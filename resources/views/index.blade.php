e12d3c8a8eca3786ca9637235ce0c1b29e7c3e97
ITFakeFace <dthung6604@gmail.com>
2024-02-21 21:24:49 +0700

    Navbar commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,23 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/src/css/home.css">
+    @include('layout.include')
+
 </head>
 
 <body>
-    {{-- Start Navbar  --}}
-    <nav class="navbar">
-        <ul>
-            <li><a href="">Logo</a></li>
-            <li><a href="">TRANG CHỦ</a></li>
-            <li><a href="">GIỚI THIỆU</a></li>
-            <li><a href="">KHÓA HỌC</a></li>
-            <li><a href="">CHÍNH SÁCH GIÁ</a></li>
-        </ul>
-        <hr>
-    </nav>
-    {{-- End Navbar --}}
-    
+    @include('layout.header')
+
 </body>
 
 </html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,27 @@
+{{-- Start Navbar  --}}
+<nav class="navbar">
+    <ul>
+        <li class="nav-item"><a href="/"><img src="/src/images/LogoCropped.jpg" alt="" width="100px"height="auto"></a></li>
+        <li class="nav-item"><a href="">TRANG CHỦ</a></li>
+        <li class="nav-item">
+            <div class="dropdown-nav-info">
+                <span class="dropdown-nav-info-btn">GIỚI THIỆU</span>
+                <div class="dropdown-content dropdown-nav-info-content">
+                    <div>
+                        <a href="#">Về Hrc Trainning Center</a>
+                    </div>
+                    <div>
+                        <a href="#">Giảng viên</a>
+                    </div>
+                    <div>
+                        <a href="#">Các khóa học</a>
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item"><a href="">KHÓA HỌC</a></li>
+        <li class="nav-item"><a href="">CHÍNH SÁCH GIÁ</a></li>
+    </ul>
+    <hr>
+</nav>
+{{-- End Navbar --}}
