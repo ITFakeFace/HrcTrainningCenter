96632aef596b4d858b5ba559bb7fec17a54d772a
ITFakeFace <dthung6604@gmail.com>
2024-03-19 11:48:56 +0700

    Homepage Changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,12 +7,71 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     @include('layout.include')
-
+    <link rel="stylesheet" href="/src/css/course.css">
+    <link rel="stylesheet" href="/src/css/newCourse.css">
 </head>
 
 <body>
     @include('layout.header')
 
+    <div style="width:100%; height: 720px; background-color: grey;">
+
+    </div>
+    <div class="inspirationBlock">
+        <div class="title">
+            <div>Đồng hành phát triển cùng nguồn nhân lực trẻ <br> - Partner of young growth</div>
+        </div>
+        <div class="description">
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo perspiciatis eius unde nam consequatur amet accusamus, ratione repellat iure tenetur saepe illum ipsum sit animi quo ullam sed corrupti rerum!</div>
+        </div>
+
+        <div class="row contentBoxBlock">
+            <div class="col-3 contentBox">
+                <div class="title">#TruyenCamHung</div>
+                <div class="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus excepturi reprehenderit magni expedita, recusandae labore iste sapiente error eos, beatae autem eaque! Commodi sunt sequi ipsa nam laborum, ad rerum.</div>
+            </div>
+            <div class="col-3 contentBox">
+                <div class="title">#TuDuyChienLuoc</div>
+                <div class="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus excepturi reprehenderit magni expedita, recusandae labore iste sapiente error eos, beatae autem eaque! Commodi sunt sequi ipsa nam laborum, ad rerum.</div>
+            </div>
+            <div class="col-3 contentBox">
+                <div class="title">#CongDong</div>
+                <div class="content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus excepturi reprehenderit magni expedita, recusandae labore iste sapiente error eos, beatae autem eaque! Commodi sunt sequi ipsa nam laborum, ad rerum.</div>
+            </div>
+        </div>
+    </div>
+    <div class="row courseBlock">
+        <div class="col-md-4">
+            <div class="courseImage">
+                <img src="#" class="courseImage">
+            </div>
+            <div class="courseTitle">Course Title</div>
+            <div class="courseContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum distinctio sapiente, ut reiciendis tempore voluptatum. Sit ipsa fugit iste modi explicabo! Facere facilis quo quasi soluta ab, hic doloribus! Ratione?</div>
+            <a href="#"><button class="courseButton">Tìm hiểu thêm</button></a>
+        </div>
+        <div class="col-md-4">
+            <div class="courseImage">
+                <img src="#" class="courseImage">
+            </div>
+            <div class="courseTitle">Course Title</div>
+            <div class="courseContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum distinctio sapiente, ut reiciendis tempore voluptatum. Sit ipsa fugit iste modi explicabo! Facere facilis quo quasi soluta ab, hic doloribus! Ratione?</div>
+            <a href="#"><button class="courseButton">Tìm hiểu thêm</button></a>
+        </div>
+        <div class="col-md-4">
+            <div class="courseImage">
+                <img src="#" class="courseImage">
+            </div>
+            <div class="courseTitle">Course Title</div>
+            <div class="courseContent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum distinctio sapiente, ut reiciendis tempore voluptatum. Sit ipsa fugit iste modi explicabo! Facere facilis quo quasi soluta ab, hic doloribus! Ratione?</div>
+            <a href="#"><button class="courseButton">Tìm hiểu thêm</button></a>
+        </div>
+    </div>
+    <div class="row newCourseBlock">
+        <div class="newCourseTitle">Các khóa học mới</div>
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit placeat fugit consectetur alias sint assumenda a quia laborum impedit deserunt pariatur eos dolores nesciunt, officia expedita cumque quidem maiores provident.
+        </div>
+    </div>
 </body>
 
 </html>
